4021e2876abdb099305dfa1fd696c9b98321e740
Adam <akrasuski1@gmail.com>
2017-10-19 01:49:21 +0200

    add simple brute force script for reference

---
+++ brute.py
@@ -0,0 +1,25 @@
+
+
+def read_file(name):
+    lines = open(name).readlines()
+    cnt, dim = [int(s) for s in lines[0].split()]
+
+    data = []
+    for i in range(cnt):
+        data.append([float(s) for s in lines[i+1].split()])
+
+    return data
+
+data = read_file("input")
+queries = read_file("queries")
+
+for qnum, q in enumerate(queries):
+    values = []
+    for vnum, v in enumerate(data):
+        s = sum(a * b for a, b in zip(q, v))
+        values.append( (s, vnum) )
+
+    print "Best for query", qnum
+    for val in sorted(values)[::-1]:
+        print val[1], "\t", val[0]
+    print
