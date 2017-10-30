80a78ab41a01cecd7ec6ad59b13cf3101780109b
Adam <akrasuski1@gmail.com>
2017-10-30 18:33:13 +0100

    Use faiss index to assign centroids - speedup

--- src/common.cpp
+++ src/common.cpp
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #include "../faiss/Clustering.h"
+#include "../faiss/IndexFlat.h"
 #include "../faiss/utils.h"
 
 
@@ -8,6 +9,8 @@ kmeans_result perform_kmeans(const FlatMatrix<float>& matrix, size_t k) {
     kmeans_result kr;
     kr.centroids.resize(k, matrix.vector_length);
     kr.assignments.resize(matrix.vector_count());
+	std::vector<float> dist(matrix.vector_count());
+	std::vector<faiss::Index::idx_t> assignments(matrix.vector_count());
 
     faiss::kmeans_clustering(
         matrix.vector_length, 
@@ -17,24 +20,13 @@ kmeans_result perform_kmeans(const FlatMatrix<float>& matrix, size_t k) {
         kr.centroids.data.data()
     );
 
-    // TODO: I think the assignments could be somehow taken out from faiss
-    // - it stores them while computing centroids anyway. I don't see any API
-    // to do that though...
-    
-    // Brute force assignment - for each vector, find closest centroid.
-    for (size_t i = 0; i < matrix.vector_count(); i++) {
-        float best = std::numeric_limits<float>::max();
+	faiss::IndexFlatL2 index(matrix.vector_length);
+	index.add(kr.centroids.vector_count(), kr.centroids.data.data());
+	index.search(matrix.vector_count(), matrix.data.data(), 1,
+			dist.data(), assignments.data());
 
-        for (size_t c = 0; c < k; c++) {
-            float dist = faiss::fvec_L2sqr(
-                    matrix.row(i), 
-                    kr.centroids.row(c),
-                    matrix.vector_length);
-            if (dist < best) {
-                kr.assignments[i] = c;
-                best = dist;
-            }
-        }
+    for (size_t i = 0; i < matrix.vector_count(); i++) {
+		kr.assignments[i] = assignments[i];
     }
     return kr;
 }
