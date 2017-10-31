610e7038cbceba56c540c7085bd6ece837e9cbfd
franc <franek.walkowiak@gmail.com>
2017-10-31 21:03:19 +0100

    update scripts

--- tests/draw.py
+++ tests/draw.py
@@ -10,7 +10,8 @@ with open('output.txt', 'r') as f:
 # ds = data series
 time_ds = 6
 #memory_ds = 9
-recall_ds = 4 # recall at 1
+recall_ds = 4 # recall at 10
+inter_ds = 7 # intersection
 number_of_queries = 100
 """
 methods = set([x[0] for x in inlist])
@@ -52,6 +53,7 @@ maximum_time = float(max([float(d[time_ds+1]) for d in inlist]))
 #minimum_mem = float(min([float(d[memory_ds+1]) for d in inlist]))
 #maximum_mem = float(max([float(d[memory_ds+1]) for d in inlist]))
 minimum_recall = float(min([float(d[recall_ds+1]) for d in inlist]))
+maximum_recall = float(max([float(d[recall_ds+1]) for d in inlist]))
 
 data = []
 for met in ['ivf', 'kmeans', 'quant']:
@@ -69,30 +71,34 @@ fig = plt.figure()
 plt.suptitle('Performance of algorithms')
 host = host_subplot(111, axes_class=AA.Axes)
 plt.subplots_adjust(right=0.85)
-#par1 = host.twinx() # parasite axis on right
+par1 = host.twinx() # parasite axis on right
 
 box = host.get_position()
-host.set_position([box.x0, box.y0 + box.height * 0.2,
-                 box.width, box.height * 0.85])
+host.set_position([box.x0, box.y0 + box.height * 0.2, box.width, box.height * 0.85])
+host.set_xscale('log')
 
 # set limits on axes
-host.set_ylim(0.99 * 1000 * minimum_time/number_of_queries, 1.01 * 1000 * maximum_time/number_of_queries)
+host.set_xlim(0.99 * 1000 * minimum_time/number_of_queries, 1.01 * 1000 * maximum_time/number_of_queries)
 #par1.set_ylim(0.99 * minimum_mem, 1.01 * maximum_mem)
-host.set_xlim(minimum_recall, 1.005)
+host.set_ylim(minimum_recall, 1.005)
 #par1.set_ylim(0, 1.01 * maximum_mem)
 
-label_time = ['Search time - ivf', 'Search time - kmeans', 'Search time - quant']
+#label_time = ['Search time - ivf', 'Search time - kmeans', 'Search time - quant']
 #label_mem = ['Memory usage - ivf', 'Memory usage - kmeans', 'Memory usage - quant']
-marker_time = ['C1o', 'C3o', 'C5o']
-#marker_mem = ['C2s', 'C4s', 'C6s']
+label_recall = ['Recall@10 - ivf', 'Recall@10 - kmeans', 'Recall@10 - quant']
+label_inter = ['Top-100 intersection - ivf', 'Top-100 intersection - kmeans', 'Top-100 intersection - quant']
+marker_recall = ['C1o', 'C3o', 'C5o']
+marker_inter = ['C2s', 'C4s', 'C6s']
 
 for i in [0, 1, 2]:
-    host.plot([d[recall_ds] for d in data[i]], [float(d[time_ds])*1000/number_of_queries   for d in data[i]], marker_time[i], label = label_time[i], markersize=1.5)
-    #par1.plot([d[recall_ds] for d in data[i]], [d[memory_ds] for d in data[i]], marker_mem[i], label = label_mem[i], arkersize=2.8)
+    #host.plot([d[recall_ds] for d in data[i]], [float(d[time_ds])*1000/number_of_queries   for d in data[i]], marker_time[i], label = label_time[i], markersize=1.5)
+    host.plot([float(d[time_ds])*1000/number_of_queries for d in data[i] ], [d[recall_ds] for d in data[i]], marker_recall[i], label = label_recall[i], markersize=1.4)
+    par1.plot([float(d[time_ds])*1000/number_of_queries for d in data[i] ], [d[inter_ds] for d in data[i]], marker_inter[i], label = label_inter[i], markersize=1.4)
+    #par1.plot([d[recall_ds] for d in data[i]], [d[memory_ds] for d in data[i]], marker_mem[i], label = label_mem[i], markersize=2.8)
 
-host.set_xlabel("Recall@10")
-host.set_ylabel("Time per query [ms]")
-#par1.set_ylabel("Max. memory usage [MB]")
+host.set_xlabel("Time per query [ms]")
+host.set_ylabel("Recall@10")
+par1.set_ylabel("Top-100 intersection")
 
 host.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2)
 fig.savefig('plot.svg')
--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Please run this script from repo's main directory.
 
-output_file="output2.txt"
+output_file="output.txt"
 rm -f "$output_file"
 
 comm_bin="bin/bench"
@@ -14,24 +14,24 @@ quant_bin=$(echo $comm_bin)_quantization
 # TODO decide which arguments we'll test
 ivf_args=(1 2 4 8 16 32 64 128 256 512 1024 2048) #16 32 64 128 256 512 1024) # nprobe
 alsh_args_1=(2 3 4 6 8 12 16 24 32) # number of hash tables
-alsh_args_2=(2 3 4 6 8 12 16 24 32) # number of hash function in one hash table
+alsh_args_2=(2 3 4 6 8 12 16 24 32) # number of hash functions in one hash table
 kmeans_args_1=(3) # number of additional components (m)
 kmeans_args_2=(2 3 4 5 6 7 8 9) # layers count
 kmeans_args_3="10 20 30 40 50 60 70 80"
 kmeans_args_arr=(10 20 30 40 50 60 70 80)
-quant_args_1=(16 32 48 64 128) # subspace count
+quant_args_1=(16 32 64 128) # subspace count
 quant_args_2=(16 32 64) # centroid count
 
-line_id=('R@1 = ' 'R@10 = ' 'R@100 = ' 'Search time')
+line_id=('R@1 = ' 'R@10 = ' 'R@100 = ' 'Search time' 'Intersection')
 
 write_results() {
-    for i in {0..3}; do results[i]=$(echo "$res" | grep -E "${line_id[i]}"); done
+    for i in {0..4}; do results[i]=$(echo "$res" | grep -E "${line_id[i]}"); done
     if [[ "$1" == "kmeans" ]]; then
         #for i in {0..3}; do echo "${results[i]}"; done
         i=1
         for opened_trees in "${kmeans_args_arr[@]}"; do
             printf "%s %s %s " "$1" "$2" "$opened_trees" >> "$output_file"
-            for j in {0..3}; do
+            for j in {0..4}; do
                  printf "%s " $(echo "${results[j]}" | sed "${i}q;d" | awk '{print $NF}') >> "$output_file"
             done
             printf "\n" >> "$output_file"
@@ -39,32 +39,36 @@ write_results() {
         done
     else
         printf "%s" "$1" >> "$output_file"
-        for i in {0..4}; do printf " %s" $(echo "${results[i]}" | awk '{print $NF}' ) >> "$output_file" ; done
+        for i in {0..5}; do printf " %s" $(echo "${results[i]}" | awk '{print $NF}' ) >> "$output_file" ; done
         printf "\n" >> "$output_file"
     fi
 }
 
-#for a in "${ivf_args[@]}"; do
-#    res="$($time_path $ivf_bin $a 2>&1)"
-#    write_results "ivf $a 0 0"
-#done
+for a in "${ivf_args[@]}"; do
+    echo "Running ivf $a"
+    res="$($time_path $ivf_bin $a 2>&1)"
+    write_results "ivf $a 0 0"
+done
 
 #for a in "${alsh_args_1[@]}"; do
 #    for b in "${alsh_args_2[@]}"; do
+#        echo "Running alsh $a $b"
 #        res="$($time_path $alsh_bin $a $b 2>&1)"
 #        write_results "alsh $a $b 0"
 #    done
 #done
 
-#for a in "${kmeans_args_1[@]}"; do
-#    for b in "${kmeans_args_2[@]}"; do
-#            res="$($time_path $kmeans_bin $a $b $kmeans_args_3 2>&1)"
-#            write_results "kmeans" "$a $b"
-#    done
-#done
+for a in "${kmeans_args_1[@]}"; do
+    for b in "${kmeans_args_2[@]}"; do
+            echo "Running kmeans $a $b $kmeans_args_3"
+            res="$($time_path $kmeans_bin $a $b $kmeans_args_3 2>&1)"
+            write_results "kmeans" "$a $b"
+    done
+done
 
 for a in "${quant_args_1[@]}"; do
     for b in "${quant_args_2[@]}"; do
+            echo "Running quant $a $b"
             res="$($time_path $quant_bin $a $b 2>&1)"
             write_results "quant $a $b 0"
     done
