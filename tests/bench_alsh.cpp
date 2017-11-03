c8c3e7b67233cef8f0742aaf7802ec321a98b491
franc <franek.walkowiak@gmail.com>
2017-11-04 00:29:05 +0100

    adapt alsh and kmeans for testing

--- tests/bench_alsh.cpp
+++ tests/bench_alsh.cpp
@@ -2,9 +2,12 @@
 #include "../src/bench.h"
 #include "../src/alsh.h"
 
-size_t m = 3;
-int augtype = 2;
-float U = 1.0;
+size_t m = 3; // additional vector dimensions
+int augtype; // augmentation type
+size_t L; // number of hash tables
+size_t K; // number of hash functions in one hash table
+float r; // hash function parameter
+float U; // vector scaling coefficient
 
 faiss::Index* get_trained_index(const FloatMatrix& xt) {
     MipsAugmentation* aug;
@@ -15,13 +18,22 @@ faiss::Index* get_trained_index(const FloatMatrix& xt) {
     case 2: aug = new MipsAugmentationNone(dim); break;
     default: exit(1);
     }
-    faiss::Index* index = new IndexALSH(dim, 6, 32, 2.5, aug);
+    faiss::Index* index = new IndexALSH(dim, L, K, r, aug);
     index->train(xt.vector_count(), xt.data.data());
     return index;
 }
 
 int main(int argc, char **argv) {
-    faiss::Index* index = bench_train(get_trained_index);
-    bench_add(index);
-    bench_query(index);
+    if (argc < 5) {
+        printf("Arguments missing, terminating.\n");
+    } else {
+        L = atoi(argv[1]);
+        K = atoi(argv[2]);
+        sscanf(argv[3], "%f", &r);
+        augtype = atoi(argv[4]);
+        sscanf(argv[5], "%f", &U);
+        faiss::Index* index = bench_train(get_trained_index);
+        bench_add(index);
+        bench_query(index);
+    }
 }
--- tests/bench_kmeans.cpp
+++ tests/bench_kmeans.cpp
@@ -2,11 +2,17 @@
 #include "../src/bench.h"
 #include "../src/kmeans.h"
 
-size_t m;
-float U;
+// arguments guide (OT = opened_trees)
+//argc=  0            1         2     3     4     5
+// bench_kmeans layers_count augtype 
+//                              1     U    OT1   OT2  ...
+//                             0/2   -1    OT1   OT2  ...
+
+size_t m = 3; // additional vector dimensions
+float U; // vector scaling coefficient
 size_t layers_count;
 size_t opened_trees;
-int augtype = 1;
+int augtype; // augmentation type
 
 faiss::Index* get_trained_index(const FloatMatrix& xt) {
     MipsAugmentation* aug;
@@ -26,10 +32,9 @@ int main(int argc, char **argv) {
     if (argc < 5) {
         printf("Arguments missing, terminating.\n");
     } else {
-        m = atoi(argv[1]);
-        layers_count = atoi(argv[2]);
+        layers_count = atoi(argv[1]);
+        augtype = atoi(argv[2]);
         sscanf(argv[3], "%f", &U);
-        opened_trees = atoi(argv[4]);
 
         faiss::Index* index = bench_train(get_trained_index);
         bench_add(index);
