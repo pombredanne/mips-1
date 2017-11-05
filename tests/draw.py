c3d8168eb586b0184c81624e3b3c876570a46585
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2017-11-05 13:52:04 +0100

    Update drawing script with better argument parsing and different marker settings

--- tests/draw.py
+++ tests/draw.py
@@ -3,66 +3,100 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
+import argparse
 
-if len(sys.argv) < 2:
-    print('Sorry, I don\'t know what to draw.')
 
-with open('output.txt', 'r') as f:
-    reader = csv.reader(f, delimiter="\t")
-    inlist = list(reader)
+MARKER_SIZE = 3.5
+WIDTH   = 0.5
+TIME_DS   = 8
+RECALL_DS = 6  # recall at 10
+INTER_DS  = 9  # intersection
+N_QUERIES = 100
 
-# ds = data series
-time_ds = 8
-recall_ds = 6 # recall at 10
-inter_ds = 9 # intersection
-number_of_queries = 100
 
-minimum_time = float(min([float(d[time_ds+1]) for d in inlist]))
-maximum_time = float(max([float(d[time_ds+1]) for d in inlist]))
-minimum_recall = float(min([float(d[recall_ds+1]) for d in inlist]))
-maximum_recall = float(max([float(d[recall_ds+1]) for d in inlist]))
-minimum_inter = float(min([float(d[inter_ds+1]) for d in inlist]))
-maximum_inter = float(max([float(d[inter_ds+1]) for d in inlist]))
+def plot_recall(fig, host, data, limits):
+    labels = ['Recall@10 - ivf', 'Recall@10 - kmeans', 'Recall@10 - quant', 'Recall@10 - alsh']
+    markers = ['ro', 'yo', 'bo', 'go']
 
-data = []
-for met in ['ivf', 'kmeans', 'quant', 'alsh']:
-    data.append([x[1:] for x in inlist if x[0] == met])
-
-# prepare the plot
-fig = plt.figure()
-host = host_subplot(111)
-box = host.get_position()
-host.set_position([box.x0, box.y0 + box.height*0.35, box.width, box.height * 0.7])
-host.set_xscale('log')
-host.set_xlabel("Time per query [ms]")
-m_size=1.6
-
-# set limits on axes
-host.set_xlim(0.99 * 1000 * minimum_time/number_of_queries,
-              1.01 * 1000 * maximum_time/number_of_queries)
-
-if sys.argv[1] == 'recall':
     plt.suptitle('Recall vs. time')
-    labels = ['Recall@10 - ivf', 'Recall@10 - kmeans',
-              'Recall@10 - quant', 'Recall@10 - alsh']
-    markers = ['C1o', 'C3o', 'C5o', 'C7o']
     host.set_ylabel("Recall@10")
-    for i in [0, 1, 2, 3]:
-        host.plot([float(d[time_ds])*1000/number_of_queries for d in data[i]],
-                 [d[recall_ds] for d in data[i]],
-                 markers[i], label = labels[i], markersize=m_size)
+    host.set_ylim(0.9 * limits[0], 1.1 * limits[1])
+
+    for i in range(4):
+        x = [float(d[TIME_DS]) * 1000 / N_QUERIES for d in data[i]]
+        y = [d[RECALL_DS] for d in data[i]]
+
+        host.plot(x, y, markers[i], label=labels[i],
+                  markersize=MARKER_SIZE, mfc='none', markeredgewidth=WIDTH)
+
     host.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2)
     fig.savefig('plot_recall.pdf')
 
-if sys.argv[1] == 'inter':
-    plt.suptitle('Top-100 intersection vs. time')
+
+def plot_intersect(fig, host, data, limits):
     labels = ['Top-100 intersection - ivf', 'Top-100 intersection - kmeans',
               'Top-100 intersection - quant', 'Top-100 intersection - alsh']
-    markers = ['C2s', 'C4s', 'C6s', 'C8s']
+    markers = ['rs', 'ys', 'bs', 'gs']
+
+    plt.suptitle('Top-100 intersection vs. time')
     host.set_ylabel("Top-100 intersection")
-    for i in [0, 1, 2, 3]:
-        host.plot([float(d[time_ds])*1000/number_of_queries for d in data[i]],
-                 [d[inter_ds] for d in data[i]],
-                 markers[i], label = labels[i], markersize=m_size)
+    host.set_ylim(0.9 * limits[0], 1.1 * limits[1])
+
+    for i in range(4):
+        x = [float(d[TIME_DS]) * 1000 / N_QUERIES for d in data[i]]
+        y = [d[INTER_DS] for d in data[i]]
+
+        host.plot(x, y, markers[i], label=labels[i],
+                  markersize=MARKER_SIZE, mfc='none', markeredgewidth=WIDTH)
+
     host.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2)
     fig.savefig('plot_intersection.pdf')
+
+
+def main(args):
+
+    with open(args.input) as f:
+        reader = csv.reader(f, delimiter="\t")
+        inlist = list(reader)
+
+    minimum_time   = min([float(d[TIME_DS + 1]) for d in inlist])
+    maximum_time   = max([float(d[TIME_DS + 1]) for d in inlist])
+    minimum_recall = min([float(d[RECALL_DS + 1]) for d in inlist])
+    maximum_recall = max([float(d[RECALL_DS + 1]) for d in inlist])
+    minimum_inter  = min([float(d[INTER_DS + 1]) for d in inlist])
+    maximum_inter  = max([float(d[INTER_DS + 1]) for d in inlist])
+
+    data = []
+    for met in ['ivf', 'kmeans', 'quant', 'alsh']:
+        data.append([x[1:] for x in inlist if x[0] == met])
+
+    # prepare the plot
+    fig = plt.figure()
+    host = host_subplot(111)
+    box = host.get_position()
+    host.set_position([box.x0, box.y0 + box.height*0.35, box.width, box.height * 0.7])
+    host.set_xscale('log')
+    host.set_xlabel("Time per query [ms]")
+
+    # set limits on axes
+    host.set_xlim(0.9 * 1000 * minimum_time / N_QUERIES,
+                  1.1 * 1000 * maximum_time / N_QUERIES)
+
+    if args.mode == 'recall':
+        plot_recall(fig=fig, host=host, data=data, limits=(minimum_recall, maximum_recall))
+
+    if args.mode == 'inter':
+        plot_intersect(fig=fig, host=host, data=data, limits=(minimum_inter, maximum_inter))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("A simple utility to create plots with benchmark results.")
+    parser.add_argument("--input", default="output.txt",
+                        help="File from which the data should be read.")
+    parser.add_argument("--mode", choices={"recall", "inter"}, default="recall",
+                        help="Controls what plot will be drawn")
+    args = parser.parse_args()
+
+    main(args)
+
+
